1bcbf7b4cd19836dd24cf5cb1e1a6d1ebb44d605
Toni Kangas <kangasta@users.noreply.github.com>
2023-10-03 23:58:54 +0300

    Also provide build outputs in archives (#2)

--- .ci/build-index-files.sh
+++ .ci/build-index-files.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -xe
 
 target=${1:-out}
+name=${NAME:-cow}
 
 rm -rf $target
 mkdir -p $target
 cp index.* SourceSans3.ttf $target
 
 sed -i "s/build_version_note/${VERSION_NOTE:0:11}/" $target/index.*
-sed -i "s/build_emoji/${EMOJI:-🐄}/" $target/index.*
+sed -i "s/build_emoji/${EMOJI:-🐮}/" $target/index.*
 sed -i "s/build_hello/${HELLO:-Moo}/" $target/index.*
-sed -i "s/build_name/${NAME:-cow}/" $target/index.*
+sed -i "s/build_name/$name/" $target/index.*
+
+tar -czvf animals-$name.tar.gz $target/*
